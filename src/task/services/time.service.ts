a3bf3bffb38c0063c8b2307623d1fe46fc0a3455
yengobilly4063 <yengobilly4063@gmail.com>
2022-04-25 00:16:43 +0300

    tiny change

--- src/task/services/time.service.ts
+++ src/task/services/time.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { Between, MoreThan } from 'typeorm';
+import { Between, LessThan, MoreThan } from 'typeorm';
 import { subMonths, addMonths } from 'date-fns';
 
 @Injectable()
 export class TimeService {
   isEighteenMonghtsOlder(date: Date) {
-    return MoreThan(subMonths(date, 18));
+    return LessThan(subMonths(date, 18));
   }
   isBetweenTwelveAndEighteenMonths(date: Date) {
     return Between(addMonths(date, 12), addMonths(date, 18));
